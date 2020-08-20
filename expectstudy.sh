b173dd7ede73ef51459a406b8cb7f14cc3c2c3d2
szw <z52linux@gmail.com>
2020-08-20 21:16:49 +0800

    add linux.pptx

--- expectstudy.sh
+++ expectstudy.sh
@@ -10,8 +10,8 @@ spawn git push origin master
 set username [ lindex $argv 0 ]
 set password [ lindex $argv 1 ]
 expect {
-"Username" { send "$username\r"; exp_continue }
-"Password" { send "$password\r" }
+    "Username" { send "$username\r"; exp_continue }
+    "Password" { send "$password\r" }
 }
 interact
 EOF

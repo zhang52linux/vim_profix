309d26922fdc97258c4d407908aeaccf1d7e7433
szw <z52linux@gmail.com>
2020-08-20 21:15:09 +0800

    add linux.pptx

--- expectstudy.sh
+++ expectstudy.sh
@@ -3,15 +3,7 @@
 git add *
 git commit -m "add linux.pptx"
 
-
-
 /usr/bin/expect <<-EOF 
-if {$argc < 2} {
-    send_user "Usage:\n  $argv0 IPaddr User Passwd Port Passphrase\n"
-    puts stderr "argv error!\n"
-    sleep 1
-    exit 1
-}
 spawn git push origin master
 set username [ lindex $argv 0 ]
 set password [ lindex $argv 1 ]

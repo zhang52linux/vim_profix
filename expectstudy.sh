0ab6890b37084ddf7ac101e7a601f041ec4c206b
szw <z52linux@gmail.com>
2020-08-20 21:24:49 +0800

    add linux.pptx

--- expectstudy.sh
+++ expectstudy.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 git add *
-git status
-sleep 3
 git commit -m "add linux.pptx"
 
+
+while read username password 
+do
 /usr/bin/expect <<-EOF 
 spawn git push origin master
-set username [ lindex $argv 0 ]
-set password [ lindex $argv 1 ]
 expect {
     "Username" { send "$username\r"; exp_continue }
     "Password" { send "$password\r" }
 }
 interact
 EOF
+done < ~/passwd.txt
 
 
 

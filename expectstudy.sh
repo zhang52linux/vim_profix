53dae2d68028a175122d3fef452ed0dad9c54162
szw <z52linux@gmail.com>
2020-08-22 20:50:23 +0800

    add linux.pptx

--- expectstudy.sh
+++ expectstudy.sh
@@ -10,6 +10,7 @@ while read username password
 do
 /usr/bin/expect <<-EOF 
 spawn git push origin master
+set timeout 30
 expect {
     "Username" { send "$username\r"; exp_continue }
     "Password" { send "$password\r" }

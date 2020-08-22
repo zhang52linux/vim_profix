c28407816dd520a9811d47e6ba263abfaec09234
szw <z52linux@gmail.com>
2020-08-22 20:47:25 +0800

    add linux.pptx

---
+++ myshell/autoAddReposition.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+sudo echo '' > /etc/apt/sourceslist
+
+
+sudo cat > /etc/apt/sources.list <<EOF  
+# 阿里源
+deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+EOF
+
+
+sudo apt update
+
+sudo apt upgrade
